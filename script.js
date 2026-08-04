59d44878ea1a1e2b3a326ff45dced3e664ce50d2
ffgc6hn4bg-beep <ffgc6hn4bg@privaterelay.appleid.com>
2026-08-04 21:03:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
-const birthday = new Date("2026-08-15T00:00:00").getTime();
+let birthday = new Date(2026, 7, 15, 0, 0, 0).getTime();
 
-const timer = setInterval(() => {
+setInterval(function(){
 
-    const now = new Date().getTime();
+    let now = new Date().getTime();
 
-    const distance = birthday - now;
-
-
-    if (distance <= 0) {
-
-        document.getElementById("timer").innerHTML = 
-        "Happy Birthday Malak ❤️";
-
-        document.getElementById("nextBtn").disabled = false;
-
-        clearInterval(timer);
-
-        return;
-    }
+    let distance = birthday - now;
 
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-    const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) /
-        (1000 * 60 * 60)
-    );
+    let hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
 
-    const minutes = Math.floor(
-        (distance % (1000 * 60 * 60)) /
-        (1000 * 60)
-    );
+    let minutes = Math.floor((distance / (1000 * 60)) % 60);
 
-    const seconds = Math.floor(
-        (distance % (1000 * 60)) /
-        1000
-    );
+    let seconds = Math.floor((distance / 1000) % 60);
 
 
     document.getElementById("days").innerHTML = days;
