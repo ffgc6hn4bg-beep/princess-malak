4ad5ff743b23d1ce2b9a60c847fbf6ba00ca08f3
ffgc6hn4bg-beep <ffgc6hn4bg@privaterelay.appleid.com>
2026-08-04 22:28:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,29 +3,24 @@ let birthday = new Date(2026, 7, 15, 0, 0, 0).getTime();
 
 function updateTimer(){
 
-let now = new Date().getTime();
+    let now = new Date().getTime();
 
-let distance = birthday - now;
-
-
-let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-
-let hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
-
-let minutes = Math.floor((distance / (1000 * 60)) % 60);
-
-let seconds = Math.floor((distance / 1000) % 60);
+    let distance = birthday - now;
 
 
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-document.getElementById("days").innerHTML = days;
+    let hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
 
-document.getElementById("hours").innerHTML = hours;
+    let minutes = Math.floor((distance / (1000 * 60)) % 60);
 
-document.getElementById("minutes").innerHTML = minutes;
+    let seconds = Math.floor((distance / 1000) % 60);
 
-document.getElementById("seconds").innerHTML = seconds;
 
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
 
 }
 
@@ -35,8 +30,7 @@ setInterval(updateTimer,1000);
 updateTimer();
 
 
-
-// فتح NEXT مؤقتًا عشان نكمل التصميم
+// فتح NEXT مؤقتًا أثناء التصميم
 let next = document.getElementById("nextBtn");
 
 next.disabled = false;
@@ -44,10 +38,13 @@ next.disabled = false;
 next.style.opacity = "1";
 
 next.style.cursor = "pointer";
-document.getElementById("nextBtn").onclick = function(){
 
-    document.querySelector(".container").style.display="none";
 
-    document.getElementById("giftPage").style.display="block";
+// الانتقال لصفحة الهدية
+next.onclick = function(){
+
+    document.querySelector(".container").style.display = "none";
+
+    document.getElementById("giftPage").style.display = "block";
 
 };
